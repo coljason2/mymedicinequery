59b9835de7d9e45c3637485caf173662d8a9cc47
TW\junchi <junchi@ctech.com.my>
2018-04-26 17:58:52 +0800

    靜態資源不擋

--- src/main/com/com/config/SecurityConfiguration.java
+++ src/main/com/com/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/resources/**", "/login").permitAll()
+		http.authorizeRequests().antMatchers("/resources/**","/layout/**", "/login").permitAll()
 				.antMatchers("/home", "/get/**", "/query/**").access("hasRole('USER')").and().httpBasic().and()
 				.formLogin().usernameParameter("username").passwordParameter("password").loginPage("/login")
 				.defaultSuccessUrl("/home").and().logout().logoutSuccessUrl("/login?logout").and().exceptionHandling()
