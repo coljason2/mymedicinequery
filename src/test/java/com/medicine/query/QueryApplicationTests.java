c007c59d7daf166f22bd1ed5b884a3b5d936a082
jason.nb <jason.chen@dkdtrillion.com>
2022-12-08 15:39:56 +0800

    add log

--- src/test/java/com/medicine/query/QueryApplicationTests.java
+++ src/test/java/com/medicine/query/QueryApplicationTests.java
@@ -15,6 +15,7 @@ class QueryApplicationTests {
     @Autowired
     ObjectMapper mapper;
 
+
     @Test
     public void JsonToBean() throws JsonProcessingException {
         String json = "{\"Hash\":\"39f4905e-48c4-4487-bd78-ed3c5a59795a\",\"FileName\":\"3e59be35-7615-40b9-a7eb-1ec05fbd80bameds.pdf\",\"Size\":0,\"ResultCode\":\"00\",\"Message\":\"成功\"}".toLowerCase();
