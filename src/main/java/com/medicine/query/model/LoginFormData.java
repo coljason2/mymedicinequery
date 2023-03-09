1c62501420da0eb660bd3ace568d5dc6514811fb
jason.nb <jason.chen@dkdtrillion.com>
2023-03-09 14:24:56 +0800

    revert

--- src/main/java/com/medicine/query/model/LoginFormData.java
+++ src/main/java/com/medicine/query/model/LoginFormData.java
@@ -12,9 +12,7 @@ import java.util.Base64;
 public class LoginFormData {
     private String username = "TGlzYW5pdGFteQ==";
     private String password = "MzY3OTQ5OA==";
-    private String wsrc = "aI9ta3KeZJqRaFeXa5qaZ2pya2lqc6ibyZLJn6Ks3HBoj21rcp5mkpFoV5drmppnanJraWpzpqTQmKFgY2ycbnCTbmRphmWXmHBqmG6bkmY";
+    private String wsrc = "aI9tZG2XZJeRaFeXaZaSZ29uZW1xc6ibyZLJn6Ks3HBoj21kbZdlmpFoV5dplpJnb25lbXFzpqTQmKFgY2yVaWmTcGRphmWVlGhqnWqVlm0";
     private String act = "act_login";
     private String back_act = "user.php";
-
-    private String submit="";
 }
